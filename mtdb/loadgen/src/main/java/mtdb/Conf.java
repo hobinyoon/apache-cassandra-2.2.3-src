b5659543e3aa2097199e26229f754331b14d5545
Hobin Yoon <hobinyoon@gmail.com>
2015-10-29 00:59:27 -0400

    gen uniform write times

--- mtdb/loadgen/src/main/java/mtdb/Conf.java
+++ mtdb/loadgen/src/main/java/mtdb/Conf.java
@@ -4,16 +4,89 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 import org.yaml.snakeyaml.Yaml;
 
+
 public class Conf
 {
-	public static void testLoadFromStream() throws IOException {
+	public static class Global {
+		int simulated_time_in_year;
+		double simulation_time_in_min;
+		int writes;
+		String write_time_dist;
+
+		Global(Object obj_) {
+			Map obj = (Map) obj_;
+			simulated_time_in_year = Integer.parseInt(obj.get("simulated_time_in_year").toString());
+			simulation_time_in_min = Double.parseDouble(obj.get("simulation_time_in_min").toString());
+			writes = Integer.parseInt(obj.get("writes").toString());
+			write_time_dist = obj.get("write_time_dist").toString();
+		}
+
+		@Override
+		public String toString() {
+			return String.format(
+					"simulated_time_in_year: %d\n"
+					+ "simulation_time_in_min: %f\n"
+					+ "writes: %d\n"
+					+ "write_time_dist: %s\n"
+					, simulated_time_in_year
+					, simulation_time_in_min
+					, writes
+					, write_time_dist
+					);
+		}
+	}
+
+	public static class PerObj {
+		int avg_reads;
+		String num_reads_dist;
+		String read_time_dist;
+
+		PerObj(Object obj_) {
+			Map obj = (Map) obj_;
+			avg_reads = Integer.parseInt(obj.get("avg_reads").toString());
+			num_reads_dist = obj.get("num_reads_dist").toString();
+			read_time_dist = obj.get("read_time_dist").toString();
+		}
+
+		@Override
+		public String toString() {
+			return String.format(
+					"avg_reads: %d\n"
+					+ "num_reads_dist: %s\n"
+					+ "read_time_dist: %s\n"
+					, avg_reads
+					, num_reads_dist
+					, read_time_dist);
+		}
+	}
+
+	public static Global global;
+	public static PerObj per_obj;
+
+	public static void Load() throws IOException {
 		InputStream input = new FileInputStream(new File("conf/loadgen.yaml"));
 		Yaml yaml = new Yaml();
-		Object data = yaml.load(input);
-		input.close();
-		System.out.println(yaml.dump(data));
+		Map root = (Map) yaml.load(input);
+		//System.out.println(yaml.dump(root));
+		//System.out.println(root.getClass().getName());
+
+		global = new Global(root.get("global"));
+		per_obj = new PerObj(root.get("per_obj"));
+
+		//_Dump();
+	}
+
+	private static void _Dump() {
+		StringBuilder sb = new StringBuilder(1000);
+		sb.append("global:\n");
+		sb.append(global.toString().replaceAll("(?m)^", "  "));
+		sb.append("per_obj:\n");
+		sb.append(per_obj.toString().replaceAll("(?m)^", "  "));
+		System.out.println(sb);
 	}
 }
--- mtdb/loadgen/src/main/java/mtdb/LoadGen.java
+++ mtdb/loadgen/src/main/java/mtdb/LoadGen.java
@@ -1,11 +1,56 @@
 package mtdb;
 
+import java.lang.InterruptedException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
 public class LoadGen
 {
+	public static void SimulatedTime() {
+	}
+
+	public static void GenWrites() throws InterruptedException {
+		// TODO: First, Uniform. Implement others too.
+		{
+			// Simulated datetime begin and end
+			LocalDateTime dt_b = LocalDateTime.of(2010, 1, 1, 0, 0);
+			LocalDateTime dt_e = LocalDateTime.of(2010 + Conf.global.simulated_time_in_year, 1, 1, 0, 0);
+
+			ZoneId zoneId = ZoneId.systemDefault();
+			// Ignore sub-second resolution
+			long epoch_sec_b = dt_b.atZone(zoneId).toEpochSecond();
+			long epoch_sec_e = dt_e.atZone(zoneId).toEpochSecond();
+			long epoch_sec_dur = epoch_sec_e - epoch_sec_b;
+
+			System.out.printf("Simulated datetime:\n"
+					+ "  begin: %s %d\n"
+					+ "  end:   %s %d\n"
+					+ "  dur:   %d\n"
+					, dt_b, epoch_sec_b
+					, dt_e, epoch_sec_e
+					, epoch_sec_dur
+					);
+
+			System.out.printf("Write times:\n");
+			for (int i = 1; i <= Conf.global.writes; i ++) {
+				long epoch_sec = epoch_sec_b + epoch_sec_dur * i / Conf.global.writes;
+				System.out.printf("  %s %d\n",
+						LocalDateTime.ofEpochSecond(epoch_sec, 0, ZoneOffset.UTC),
+						epoch_sec);
+			}
+		}
+	}
+
 	public static void main(String[] args)
 	{
 		try {
-			Conf.testLoadFromStream();
+			// TODO: measure the execution time of this function
+			Conf.Load();
+
+			GenWrites();
+
+			// TODO: GenReads() online
 		} catch (Exception e) {
 			System.out.println("Exception: " + e);
 		}

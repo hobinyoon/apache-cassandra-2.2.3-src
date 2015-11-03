dd51cebd13d7a52e20cc3f574f7de5896ca16149
Hobin Yoon <hobinyoon@gmail.com>
2015-11-03 12:53:26 -0500

    gen data on demand

--- mtdb/loadgen/plot/num-reads-per-obj/plot.sh
+++ mtdb/loadgen/plot/num-reads-per-obj/plot.sh
@@ -5,9 +5,23 @@ set -u
 
 SRC_DIR=`dirname $BASH_SOURCE`
 
-time sort -n < num-reads-per-obj > num-reads-per-obj-sorted
+# Generate data
+FN_DATA=data-num-reads-per-obj
+FN_DATA_SORTED=$FN_DATA"-sorted"
+if [ ! -f $FN_DATA_SORTED ];
+then
+	echo "Generating data ..."
+	../../loadgen --test_num_reads_per_obj=plot/num-reads-per-obj/$FN_DATA | sed 's/^/  /'
+	echo
 
-export FN_IN=$SRC_DIR/num-reads-per-obj-sorted
+	echo "Sorting ..."
+	time sort -n < $FN_DATA > $FN_DATA_SORTED
+	echo
+fi
+
+# Plot
+echo "Plotting ..."
+export FN_IN=$SRC_DIR/$FN_DATA_SORTED
 export FN_OUT=$SRC_DIR/num-reads-per-obj.pdf
-gnuplot $SRC_DIR/_num-reads-per-obj.gnuplot
+time gnuplot $SRC_DIR/_num-reads-per-obj.gnuplot
 echo "Created "$FN_OUT

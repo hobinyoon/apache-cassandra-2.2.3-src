8b636e498b32df8fcd56bed1ab72648e7d486292
Hobin Yoon <hobinyoon@gmail.com>
2015-11-03 13:04:51 -0500

    gen data on demand

--- mtdb/loadgen/plot/obj-age-when-accessed/plot.sh
+++ mtdb/loadgen/plot/obj-age-when-accessed/plot.sh
@@ -5,9 +5,23 @@ set -u
 
 SRC_DIR=`dirname $BASH_SOURCE`
 
-time sort -k 1 -n < obj-age > obj-age-sorted
+# Generate data
+FN_DATA=data-obj-age
+FN_DATA_SORTED=$FN_DATA"-sorted"
+if [ ! -f $FN_DATA_SORTED ];
+then
+	echo "Generating data ..."
+	../../loadgen --test_obj_ages=plot/obj-age-when-accessed/$FN_DATA | sed 's/^/  /'
+	echo
 
-export FN_IN=$SRC_DIR/obj-age-sorted
+	echo "Sorting ..."
+	time sort -n < $FN_DATA > $FN_DATA_SORTED
+	echo
+fi
+
+# Plot
+echo "Plotting ..."
+export FN_IN=$SRC_DIR/$FN_DATA_SORTED
 export FN_OUT=$SRC_DIR/obj-age.pdf
 gnuplot $SRC_DIR/_obj-age.gnuplot
 echo "Created "$FN_OUT

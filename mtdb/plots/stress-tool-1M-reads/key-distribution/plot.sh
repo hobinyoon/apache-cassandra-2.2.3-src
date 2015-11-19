7f3a626845174a9e1810482ebbbfc3c2d6b9b9e8
Hobin Yoon <hobinyoon@gmail.com>
2015-11-19 15:07:36 -0500

    update

--- mtdb/plots/stress-tool-1M-reads/key-distribution/plot.sh
+++ mtdb/plots/stress-tool-1M-reads/key-distribution/plot.sh
@@ -7,6 +7,6 @@ SRC_DIR=`dirname $BASH_SOURCE`
 
 #export FN_IN=$SRC_DIR/read-keys
 export FN_IN=$SRC_DIR/read-keys-1
-export FN_OUT=$FN_IN"-dist.pdf"
+export FN_OUT=$SRC_DIR/read-keys-dist.pdf
 gnuplot $SRC_DIR/_key-dist.gnuplot
 echo "Created "$FN_OUT

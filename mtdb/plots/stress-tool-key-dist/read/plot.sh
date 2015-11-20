596dfde4f1cf2a145a7bd0f96ec4623502207c71
Hobin Yoon <hobinyoon@gmail.com>
2015-11-20 10:40:17 -0500

    read key distribution

---
+++ mtdb/plots/stress-tool-key-dist/read/plot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -u
+
+SRC_DIR=`dirname $BASH_SOURCE`
+
+export FN_IN=$SRC_DIR/keys-sequential
+export FN_OUT=$SRC_DIR/read-keys-dist.pdf
+gnuplot $SRC_DIR/_key-dist.gnuplot
+echo "Created "$FN_OUT

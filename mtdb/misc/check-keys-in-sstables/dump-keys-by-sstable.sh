c1a00d766128cf4db002df2794b71a0f3b179dd0
Hobin Yoon <hobinyoon@gmail.com>
2015-11-27 21:40:10 -0500

    parameterize. updates.

--- mtdb/misc/check-keys-in-sstables/dump-keys-by-sstable.sh
+++ mtdb/misc/check-keys-in-sstables/dump-keys-by-sstable.sh
@@ -5,8 +5,26 @@ set -u
 
 SRC_DIR=`dirname $BASH_SOURCE`
 
-for ((i=1; i <= 17; i++ ))
+if [ "$#" -ne 1 ]; then
+	printf "Usage: %s path_to_data\n" $0
+	printf "  E.g.: %s ~/work/cassandra/data/data/keyspace1/standard1-2d180380949311e5945a1d822de6a4f1\n" $0
+	exit 1
+fi
+
+DN_DATA=$1
+TABLE_ID=`basename $DN_DATA`
+
+printf "Table ID: %s\n" $TABLE_ID
+mkdir $TABLE_ID
+
+for ((i=1; ; i++ ))
 do
-	echo $i
-	time ~/work/cassandra/bin/sstablekeys ~/work/cassandra/data/data/keyspace1/standard1-8be2ccd08fb411e5b8c71d822de6a4f1/la-$i-big-Data.db > keys-$i
+	FN_SSTABLE=$DN_DATA/la-$i-big-Data.db
+	if [ ! -f "$FN_SSTABLE" ]; then
+		break
+	fi
+
+	FN_KEY=$TABLE_ID/keys-$i
+	printf "Creating %s ...\n" $FN_KEY
+	(time ~/work/cassandra/bin/sstablekeys $FN_SSTABLE > $FN_KEY) | sed 's/^/  /'
 done

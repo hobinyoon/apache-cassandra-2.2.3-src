40a5aaee7702a98a601f29437c25dad20857e4c1
Hobin Yoon <hobinyoon@gmail.com>
2016-01-20 11:30:19 -0500

    If a stored Cassandra MTDB log file is there, read from it.

--- mtdb/get-cost-latency/CassLogReader.py
+++ mtdb/get-cost-latency/CassLogReader.py
@@ -18,58 +18,84 @@ _logs = []
 
 _report_interval_ms = None
 
+
 def Read():
-	lines = []
-	found_cass_activate = False
-	lines1 = []
 	with Cons.MeasureTime("Reading Cassandra system log ..."):
-		fn = os.path.expanduser("~") + "/work/cassandra/logs/system.log"
-		Cons.P("fn=%s" % fn)
-		with open(fn) as fo:
-			for line in fo.readlines():
+		if not _ReadStoredCassMtdbLog():
+			_ReadCassMtdbLog()
+		# Parse raw lines
+		global _raw_lines, _logs
+		for line in _raw_lines:
+			_logs.append(LogEntry(line))
+
+
+def _ReadStoredCassMtdbLog():
+	# If there is a stored Cassandra MTDB log file, read the raw lines
+	fn = _StoredCassMtdbLogFilename()
+	if not os.path.isfile(fn):
+		return False
+
+	Cons.P("Reading the stored Cassandra MTDB log file %s" % fn)
+	global _raw_lines
+	with open(fn) as fo:
+		for line in fo.readlines():
+			_raw_lines.append(line.strip())
+	return True
+
+
+def _StoredCassMtdbLogFilename():
+	dn = os.path.dirname(__file__) + "/../logs/cassandra"
+	return dn + "/" + LoadgenLogReader.LogFilename()
+
+
+def _ReadCassMtdbLog():
+	lines = []
+	found_clear_acc_stat = False
+	lines1 = []
+	fn = os.path.expanduser("~") + "/work/cassandra/logs/system.log"
+	Cons.P("fn=%s" % fn)
+	with open(fn) as fo:
+		for line in fo.readlines():
+			#print line
+			if "MTDB: ClearAccStat" in line:
 				#print line
-				if "MTDB: ClearAccStat" in line:
-					#print line
-					found_cass_activate = True
-					del lines[:]
-				if "MTDB:" in line:
-					lines.append(line.strip())
+				found_clear_acc_stat = True
+				del lines[:]
+			if "MTDB:" in line:
+				lines.append(line.strip())
 
-		# Look at system.log.1.zip when "MTDB: ClearAccStat" is not found
-		if found_cass_activate == False:
-			fn = os.path.expanduser("~") + "/work/cassandra/logs/system.log.1.zip"
-			Cons.P("MTDB: ClearAccStat not found. Reading more from file %s ..." % fn)
-			with zipfile.ZipFile(fn, "r") as z:
-				for fn1 in z.namelist():
-					#Cons.P(fn1)
-					for line in z.read(fn1).split("\n"):
-						#Cons.P(line)
-						if "MTDB: ClearAccStat" in line:
-							#print line
-							found_cass_activate = True
-							del lines1[:]
-						if "MTDB:" in line:
-							lines1.append(line.strip())
+	# Look at system.log.1.zip when "MTDB: ClearAccStat" is not found
+	if found_clear_acc_stat == False:
+		fn = os.path.expanduser("~") + "/work/cassandra/logs/system.log.1.zip"
+		Cons.P("MTDB: ClearAccStat not found. Reading more from file %s ..." % fn)
+		with zipfile.ZipFile(fn, "r") as z:
+			for fn1 in z.namelist():
+				#Cons.P(fn1)
+				for line in z.read(fn1).split("\n"):
+					#Cons.P(line)
+					if "MTDB: ClearAccStat" in line:
+						#print line
+						found_clear_acc_stat = True
+						del lines1[:]
+					if "MTDB:" in line:
+						lines1.append(line.strip())
 
-		if found_cass_activate == False:
-			raise RuntimeError("Incomplete Cassandra log. No MTDB: ClearAccStat in either of system.log or system.log.1.zip")
+	if found_clear_acc_stat == False:
+		raise RuntimeError("Incomplete Cassandra log. No MTDB: ClearAccStat in either of system.log or system.log.1.zip")
 
-		if len(lines1) != 0:
-			lines1.extend(lines)
-			lines = lines1
-		#for line in lines:
-		#	print line
-		global _raw_lines
-		_raw_lines = lines
+	if len(lines1) != 0:
+		lines1.extend(lines)
+		lines = lines1
+	#for line in lines:
+	#	print line
+	global _raw_lines
+	_raw_lines = lines
 
-		_WriteToFile()
-
-		_ParseLines()
+	_WriteToFile()
 
 
 def _WriteToFile():
-	dn = os.path.dirname(__file__) + "/../logs/cassandra"
-	fn = dn + "/" + LoadgenLogReader.LogFilename()
+	fn = _StoredCassMtdbLogFilename()
 	Cons.P("Writing Cassandra MTDB log to file %s" % fn)
 	with open(fn, "w") as fo:
 		for line in _raw_lines:
@@ -202,12 +228,6 @@ class LogEntry(object):
 
 	def __str__(self):
 		return ", ".join("%s: %s" % item for item in vars(self).items())
-
-
-def _ParseLines():
-	global _logs
-	for line in _raw_lines:
-		_logs.append(LogEntry(line))
 
 
 # Keeps the last cost calc time of SSTables. Memtable cost is already

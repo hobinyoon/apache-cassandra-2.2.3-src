c20ef7481ff2575ea032605a5de03143c6cb4d65
Hobin Yoon <hobinyoon@gmail.com>
2016-02-10 08:56:20 -0500

    if fails restart Cassandra and retry

--- mtdb/loadgen/create-db.sh
+++ mtdb/loadgen/create-db.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-set -e
+#cqlsh fails sometimes
+#set -e
+
 set -u
 
 SRC_DIR=`dirname $BASH_SOURCE`
 pushd $SRC_DIR > /dev/null
 
-~/work/cassandra/bin/cqlsh -f create-keyspace.cql
+while [ 1 ]; do
+	~/work/cassandra/bin/cqlsh -f create-keyspace.cql
+	RESULT=$?
+	if [ $RESULT -eq 0 ]; then
+		break
+	else
+		DATETIME=`date +"%y%m%d-%H%M%S"`
+		echo $DATETIME" Failed. Restarting Cassandra and retrying in 30 secs ..." | tee -a create-db.log
+		../../bin/kill-cassandra.sh
+		../../bin/cassandra
+		sleep 30
+	fi
+done

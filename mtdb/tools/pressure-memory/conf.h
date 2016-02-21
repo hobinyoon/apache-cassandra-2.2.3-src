5a28f92a5082a642ab9a63f60f69afd2021c1199
Hobin Yoon <hobinyoon@gmail.com>
2016-02-21 04:37:29 +0000

    static pressuring

--- mtdb/tools/pressure-memory/conf.h
+++ mtdb/tools/pressure-memory/conf.h
@@ -3,11 +3,14 @@
 namespace Conf {
 	const int initial_pressure_free_mb = 1024;
 
-	const int pressure_free_lower_bound_mb = 96;
+	const int pressure_free_lower_bound_mb = 768;
 
-	const int cached_memory_target_mb = 24;
+	const int cached_memory_target_mb = 36;
 
 	const int free_back_off_mb = 512;
 
 	const int mem_alloc_chunk_mb = 4;
+
+	const long sleep_nsec = 100000000;
+	//                      123456789
 };

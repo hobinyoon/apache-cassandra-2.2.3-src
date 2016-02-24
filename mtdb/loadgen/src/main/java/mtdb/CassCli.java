6a26bea3457f898d51030ed10cbd9c7ed86e5bb8
Hobin Yoon <hobinyoon@gmail.com>
2016-02-24 18:35:26 +0000

    handle timeout exceptions and retry

--- mtdb/loadgen/src/main/java/mtdb/CassCli.java
+++ mtdb/loadgen/src/main/java/mtdb/CassCli.java
@@ -4,6 +4,7 @@ import java.nio.ByteBuffer;
 import java.util.concurrent.ThreadLocalRandom;
 
 import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.exceptions.*;
 import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import com.datastax.driver.core.Session;
@@ -48,24 +49,38 @@ public class CassCli extends DbCli
 
 	@Override
 	protected void DbWrite(Op op) {
-		_session.execute(String.format(
-					"INSERT INTO table1 (key, epoch_sec, c0) "
-					+ "VALUES (%d, %d, 0x%s)"
-					, op.wrs.key
-					, op.wrs.wEpochSec
-					, BuildC0(Conf.mutantsLoadgenOptions.per_obj.obj_size)
-					));
+		while (true) {
+			try {
+				_session.execute(String.format(
+							"INSERT INTO table1 (key, epoch_sec, c0) "
+							+ "VALUES (%d, %d, 0x%s)"
+							, op.wrs.key
+							, op.wrs.wEpochSec
+							, BuildC0(Conf.mutantsLoadgenOptions.per_obj.obj_size)
+							));
+				return;
+			} catch (WriteTimeoutException e) {
+				// Timeout. Retry until succeed.
+			}
+		}
 	}
 
 	@Override
 	protected void DbRead(Op op) throws InterruptedException {
-		ResultSet rs = _session.execute("SELECT * FROM table1 WHERE key=" + op.wrs.key);
-		//Cons.P(String.format("fetched %d rows", rs.all().size()));
-		int rsSize = rs.all().size();
-		if (rsSize != 1) {
-			// 0 is dangerous. It scans all SStables and some of them will cause the
-			// bloom filter false positives.
-			throw new RuntimeException("Unexpected rsSize=" + rsSize);
+		while (true) {
+			try {
+				ResultSet rs = _session.execute("SELECT * FROM table1 WHERE key=" + op.wrs.key);
+				//Cons.P(String.format("fetched %d rows", rs.all().size()));
+				int rsSize = rs.all().size();
+				if (rsSize != 1) {
+					// 0 is dangerous. It scans all SStables and some of them will cause the
+					// bloom filter false positives.
+					throw new RuntimeException("Unexpected rsSize=" + rsSize);
+				}
+				return;
+			} catch (ReadTimeoutException e) {
+				// Timeout. Retry until succeed.
+			}
 		}
 	}
 

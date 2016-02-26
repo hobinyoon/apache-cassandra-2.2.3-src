0f90ca83888bc40e4698978c18df1652d534dd52
Hobin Yoon <hobinyoon@gmail.com>
2016-02-26 13:09:59 -0500

    baseline throughput latency linear regression

---
+++ mtdb/eval/baseline/throughput-latency-linear-regression/PlotData.py
@@ -0,0 +1,241 @@
+import operator
+import os
+import re
+import sys
+
+sys.path.insert(0, "../../../util/python")
+import Cons
+import Util
+
+import Conf
+
+def Gen():
+	_LoadData()
+	_GenPlotData()
+
+
+def Throughput(storage_type, key_max_value):
+	rows = _rows_by_storage[storage_type]
+	rows.sort(key=operator.attrgetter(key_max_value))
+	r = rows[-1]
+	return float(r.writes + r.reads) * 1000.0 / r.exe_time
+
+
+def Latency(storage_type, attr_name):
+	rows = _rows_by_storage[storage_type]
+	rows.sort(key=operator.attrgetter(attr_name))
+	o = rows[-1]
+	for a in attr_name.split("."):
+		o = getattr(o, a)
+	return o
+
+
+def MaxLatency(attr_name):
+	max_latency = 0
+
+	for kmv in ["lat_r." + attr_name, "lat_w." + attr_name]:
+		for st, rows in _rows_by_storage.iteritems():
+			rows.sort(key=operator.attrgetter(kmv))
+			o = rows[-1]
+			for a in kmv.split("."):
+				o = getattr(o, a)
+			max_latency = max(max_latency, o)
+
+	return max_latency
+
+
+def AvgAvgLat(storage_type, attr_name):
+	rows = _rows_by_storage[storage_type]
+	sum_lat = 0.0
+	sum_throughput = 0.0
+	max_throughput = 0.0
+	for r in rows:
+		o = r
+		for a in attr_name.split("."):
+			o = getattr(o, a)
+		sum_lat += o
+		thr = float(r.writes + r.reads) * 1000.0 / r.exe_time
+		sum_throughput += thr
+		max_throughput = max(max_throughput, thr)
+	avg_avg_lat = sum_lat / sum_throughput
+	Cons.P("%s %s avg_avg_lat=%f" % (storage_type, attr_name, avg_avg_lat))
+	return [max_throughput, max_throughput * avg_avg_lat]
+
+
+_rows_by_storage = {}
+_fn_plot_data = None
+
+
+class _Latency:
+	def __init__(self, line):
+		self._Parse(line)
+
+	def _Parse(self, line):
+		# avg= 0.995 min=0.536 max= 666 50=   0 90=   0 95=   1 99=   3 995=   6 999=  31
+			mo = re.search(r"avg= *", line)
+			if mo == None:
+				raise RuntimeError("Unexpected format [%s]" % line)
+			line = line[mo.end():]
+			self.avg = float(line.split()[0])
+
+			mo = re.search(r"min= *", line)
+			if mo == None:
+				raise RuntimeError("Unexpected format [%s]" % line)
+			line = line[mo.end():]
+			self.min = float(line.split()[0])
+
+			mo = re.search(r"max= *", line)
+			if mo == None:
+				raise RuntimeError("Unexpected format [%s]" % line)
+			line = line[mo.end():]
+			self.max = float(line.split()[0])
+
+			mo = re.search(r"50= *", line)
+			if mo == None:
+				raise RuntimeError("Unexpected format [%s]" % line)
+			line = line[mo.end():]
+			self._50 = int(line.split()[0])
+
+			mo = re.search(r"90= *", line)
+			if mo == None:
+				raise RuntimeError("Unexpected format [%s]" % line)
+			line = line[mo.end():]
+			self._90 = int(line.split()[0])
+
+			mo = re.search(r"95= *", line)
+			if mo == None:
+				raise RuntimeError("Unexpected format [%s]" % line)
+			line = line[mo.end():]
+			self._95 = int(line.split()[0])
+
+			mo = re.search(r"99= *", line)
+			if mo == None:
+				raise RuntimeError("Unexpected format [%s]" % line)
+			line = line[mo.end():]
+			self._99 = int(line.split()[0])
+
+			mo = re.search(r"995= *", line)
+			if mo == None:
+				raise RuntimeError("Unexpected format [%s]" % line)
+			line = line[mo.end():]
+			self._995 = int(line.split()[0])
+
+			mo = re.search(r"999= *", line)
+			if mo == None:
+				raise RuntimeError("Unexpected format [%s]" % line)
+			line = line[mo.end():]
+			self._999 = int(line.split()[0])
+
+	def __str__(self):
+		return "[%s]" % (" ".join("%s=%s" % item for item in vars(self).items()))
+
+
+class ExpItem:
+	def __init__(self, raw_lines):
+		#Cons.P(raw_lines)
+		self.raw_lines = raw_lines
+		self.saturated = 0
+		self._ParseLines()
+
+	def _ParseLines(self):
+		i = 0
+		while i < len(self.raw_lines):
+			line = self.raw_lines[i]
+			#Cons.P("line=[%s]" % line)
+			if i == 0:
+				# Local SSD	160223-052200	10,000	"  # # of writes: 266670
+				t = line.split("\t")
+				if len(t) != 4:
+					raise RuntimeError("Unexpected format [%s]" % line)
+				self.storage = t[0]
+				self.exp_datetime = t[1]
+				# Not very relevant. This is from the loadgen client
+				self.num_Ws_per_simulation_time_min = t[2]
+				t1 = t[3].split("# of writes: ")
+				if len(t1) != 2:
+					raise RuntimeError("Unexpected format [%s]" % t[3])
+				self.writes = int(t1[1])
+			elif "# saturated" in line:
+				self.saturated = 2
+			elif "# of reads : " in line:
+				t = line.split("# of reads : ")
+				if len(t) != 2:
+					raise RuntimeError("Unexpected format [%s]" % line)
+				self.reads = int(t[1])
+			elif "# Write latency:" in line:
+				i += 1
+				line = self.raw_lines[i]
+				self.lat_w = _Latency(line)
+			elif "# Read latency:" in line:
+				i += 1
+				line = self.raw_lines[i]
+				self.lat_r = _Latency(line)
+			elif i == len(self.raw_lines) - 1:
+				t = line.split()
+				if (len(t) != 2) or (t[1] != "ms\""):
+					raise RuntimeError("Unexpected format [%s]" % line)
+				self.exe_time = int(t[0])
+			i += 1
+
+	def __str__(self):
+		items = []
+		for i in sorted(vars(self).items()):
+			if i[0] == "raw_lines":
+				continue
+			items.append("%s=%s" % (i[0], i[1]))
+			#Cons.P("%s:%s" % (i[0], i[1]))
+		return "[%s]" % (" ".join(items))
+
+
+def _LoadData():
+	global _rows_by_storage
+	fn = "data/%s" % Conf.Get("exp_datetime")
+	with Cons.MeasureTime("Reading data file %s ..." % fn):
+		with open(fn) as fo:
+			row_raw_lines = []
+			for line in fo.readlines():
+				if len(line) == 0:
+					continue
+				if line[0] == "#":
+					continue
+				line = line.strip()
+				if line.endswith("ms\""):
+					#Cons.P(line)
+					row_raw_lines.append(line)
+					row = ExpItem(row_raw_lines)
+					if row.saturated == 0:
+						if row.storage not in _rows_by_storage:
+							_rows_by_storage[row.storage] = []
+						_rows_by_storage[row.storage].append(row)
+					row_raw_lines = []
+				else:
+					row_raw_lines.append(line)
+		#for st, rows in _rows_by_storage.iteritems():
+		#	Cons.P("%s %s" % (st, rows))
+
+
+def _GenPlotData():
+	global _fn_plot_data
+	with Cons.MeasureTime("Generating plot data ..."):
+		_fn_plot_data = "data/baseline-throughput-latency-%s" % Conf.Get("exp_datetime")
+		with open(_fn_plot_data, "w") as fo:
+			fmt = "%13s %7d %8d %7d %8.2f" \
+					" %6.2f %4d %4d %6.2f %4d %4d" \
+					" %1d"
+			fo.write("%s\n" % Util.BuildHeader(fmt, \
+					"storage_type num_writes num_reads exe_time_ms throughput_ops_per_sec" \
+					" lat_w_avg_ms lat_w_50_ms lat_w_99_ms lat_r_avg_ms lat_r_50_ms lat_r_99_ms" \
+					" server_saturated" \
+					))
+			# Order by storage type, writes
+			for s, rows in _rows_by_storage.iteritems():
+				rows.sort(key=operator.attrgetter("writes"))
+				for r in rows:
+					fo.write((fmt + "\n") % (("\"%s\"" % s), r.writes, r.reads, r.exe_time
+						, float(r.writes + r.reads) * 1000.0 / r.exe_time
+						, r.lat_w.avg, r.lat_w._50, r.lat_w._99
+						, r.lat_r.avg, r.lat_r._50, r.lat_r._99
+						, r.saturated
+						))
+				fo.write("\n")
+		Cons.P("Created file %s %d" % (_fn_plot_data, os.path.getsize(_fn_plot_data)))

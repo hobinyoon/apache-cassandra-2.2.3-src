4732e44caff7c8629319297f23366bab930eebfc
Hobin Yoon <hobinyoon@gmail.com>
2016-02-26 11:35:38 -0500

    update

--- mtdb/ec2-setup/copy-ec2-logs.sh
+++ mtdb/ec2-setup/copy-ec2-logs.sh
@@ -1,11 +1,20 @@
-s0=54.204.71.226
-s1=54.145.133.12
-s2=54.166.92.110
-s3=54.197.124.158
-s4=54.167.170.49
+# EC2 Servers
+declare -a ec2s=(
+23.22.13.183
+23.22.38.77
+54.197.159.32
+184.73.58.105
+54.161.196.221
+54.211.16.110
+54.144.232.33
+54.211.146.69
+54.197.108.12
+54.167.151.65
+)
 
-rsync -av ubuntu@$s0:work/cassandra/mtdb/logs ~/work/cassandra/mtdb/
-rsync -av ubuntu@$s1:work/cassandra/mtdb/logs ~/work/cassandra/mtdb/
-rsync -av ubuntu@$s2:work/cassandra/mtdb/logs ~/work/cassandra/mtdb/
-rsync -av ubuntu@$s3:work/cassandra/mtdb/logs ~/work/cassandra/mtdb/
-rsync -av ubuntu@$s4:work/cassandra/mtdb/logs ~/work/cassandra/mtdb/
+for ip in "${ec2s[@]}"; do
+	echo "${ip}"
+	# Ignore fingerprint validation
+	#   http://stackoverflow.com/questions/20816547/stricthostkeychecking-not-ignoring-fingerprint-validation
+	rsync -ave "ssh -o StrictHostKeyChecking=no" ubuntu@${ip}:work/cassandra/mtdb/logs ~/work/cassandra/mtdb/
+done

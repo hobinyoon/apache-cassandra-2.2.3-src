26f82b9cc11a8b8f51d1dd10d6ba9b289c4708e9
Hobin Yoon <hobinyoon@gmail.com>
2016-03-06 21:14:23 -0500

    update

---
+++ mtdb/ec2-tools/server-mount-local-ssd-cold-to-ebs-ssd.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+set -u
+set -v
+
+sudo cp ~/work/cassandra/mtdb/ec2-tools/etc-fstab /etc/fstab
+sudo umount /mnt
+
+sudo mkdir -p /mnt/local-ssd
+sudo mount /mnt/local-ssd
+sudo chown -R ubuntu /mnt/local-ssd
+mkdir -p /mnt/local-ssd/cass-data
+mkdir -p /mnt/local-ssd/mtdb-cold
+
+sudo mkdir -p /mnt/ebs-mag
+sudo mount /mnt/ebs-mag
+sudo chown -R ubuntu /mnt/ebs-mag
+mkdir -p /mnt/ebs-mag/cass-data
+mkdir -p /mnt/ebs-mag/mtdb-cold
+
+mkdir ~/cass-data-vol
+sudo ln -s ~/cass-data-vol /mnt/ebs-ssd-gp2
+mkdir /mnt/ebs-ssd-gp2/cass-data
+
+sudo ln -s /mnt/ebs-ssd-gp2 /mnt/cold-storage
+
+sudo chown -R ubuntu /mnt/ebs-ssd-gp2
+
+sudo chown -R ubuntu /mnt/cold-storage
+sudo chown -R ubuntu /mnt/cold-storage/mtdb-cold
+
+mkdir -p ~/work/cassandra/mtdb/logs/collectl
+ln -s /mnt/local-ssd/cass-data ~/work/cassandra/data
